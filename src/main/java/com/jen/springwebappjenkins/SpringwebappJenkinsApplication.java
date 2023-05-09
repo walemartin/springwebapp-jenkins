ecb1f970f972e7afee1d98aedc7f269075761441
Olawale Olasupo <rytesoft@hotmail.com>
2023-05-09 16:45:23 +0200

    home controller

---
+++ src/main/java/com/jen/springwebappjenkins/SpringwebappJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.jen.springwebappjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebappJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringwebappJenkinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jen/springwebappjenkins/SpringwebappJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jen.springwebappjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebappJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
